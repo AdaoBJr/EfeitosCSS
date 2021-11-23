19ad3d9183e3e21667bd75a97a13a2790c2fc896
AdaoBJr <adaobjr@gmail.com>
2021-11-23 08:00:44 -0400

    style: :lipstick: created skelecton loading

---
+++ 1.Efeito de Loading/skelecton_loading/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="img loading" />
+      <h1 className="heading loading"></h1>
+      <p className="content loading"></p>
+    </div>
+  );
+}
+
+export default App;
