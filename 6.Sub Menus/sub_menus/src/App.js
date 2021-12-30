d01843e87af47f9767cc2f54539d0ac04d67a9ba
AdaoBJr <adaobjr@gmail.com>
2021-12-30 07:16:45 -0400

    feat: :sparkles: created links

--- 6.Sub Menus/sub_menus/src/App.js
+++ 6.Sub Menus/sub_menus/src/App.js
@@ -23,6 +23,86 @@ function App() {
       <footer className="root">
         <div className="wrapper">
           <div className="links">
+            <HomeOutlined className="linkIcon" />
+            <span className="linkName">Dashboard</span>
+            <CaretDownOutlined className="collapseLink" />
+            <ul className="collapseMenu">
+              <a href="#" className="collapseSublink">
+                Data
+              </a>
+              <a href="#" className="collapseSublink">
+                Group
+              </a>
+              <a href="#" className="collapseSublink">
+                Members
+              </a>
+            </ul>
+          </div>
+          <div className="links">
+            <MessageOutlined className="linkIcon" />
+            <span className="linkName">Messenger</span>
+            <CaretDownOutlined className="collapseLink" />
+            <ul className="collapseMenu">
+              <a href="#" className="collapseSublink">
+                Data
+              </a>
+              <a href="#" className="collapseSublink">
+                Group
+              </a>
+              <a href="#" className="collapseSublink">
+                Members
+              </a>
+            </ul>
+          </div>
+          <div className="links">
+            <PieChartOutlined className="linkIcon" />
+            <span className="linkName">Results</span>
+            <CaretDownOutlined className="collapseLink" />
+            <ul className="collapseMenu">
+              <a href="#" className="collapseSublink">
+                Data
+              </a>
+              <a href="#" className="collapseSublink">
+                Group
+              </a>
+              <a href="#" className="collapseSublink">
+                Members
+              </a>
+            </ul>
+          </div>
+          <div className="links">
+            <FolderOpenOutlined className="linkIcon" />
+            <span className="linkName">Projects</span>
+            <CaretDownOutlined className="collapseLink" />
+            <ul className="collapseMenu">
+              <a href="#" className="collapseSublink">
+                Data
+              </a>
+              <a href="#" className="collapseSublink">
+                Group
+              </a>
+              <a href="#" className="collapseSublink">
+                Members
+              </a>
+            </ul>
+          </div>
+          <div className="links">
+            <TeamOutlined className="linkIcon" />
+            <span className="linkName">Team</span>
+            <CaretDownOutlined className="collapseLink" />
+            <ul className="collapseMenu">
+              <a href="#" className="collapseSublink">
+                Data
+              </a>
+              <a href="#" className="collapseSublink">
+                Group
+              </a>
+              <a href="#" className="collapseSublink">
+                Members
+              </a>
+            </ul>
+          </div>
+          <div className="links">
             <SettingOutlined className="linkIcon" />
             <span className="linkName">Settings</span>
             <CaretDownOutlined className="collapseLink" />
@@ -38,11 +118,6 @@ function App() {
               </a>
             </ul>
           </div>
-          <PieChartOutlined />
-          <MessageOutlined />
-          <FolderOpenOutlined />
-          <TeamOutlined />
-          <HomeOutlined />
         </div>
       </footer>
     </main>
