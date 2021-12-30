80a97edba8f704b1e769cc4e5342829cdf4f6b3f
AdaoBJr <adaobjr@gmail.com>
2021-12-30 12:50:59 -0400

    feat: :art: updated structure

--- 6.Sub Menus/sub_menus/src/App.js
+++ 6.Sub Menus/sub_menus/src/App.js
@@ -23,9 +23,11 @@ function App() {
       <footer className="root">
         <div className="wrapper">
           <div className="links">
-            <HomeOutlined className="linkIcon" />
-            <span className="linkName">Dashboard</span>
-            <CaretDownOutlined className="collapseLink" />
+            <div className="linkName">
+              <HomeOutlined className="linkIcon" />
+              <span>Dashboard</span>
+              <CaretDownOutlined className="collapseLink" />
+            </div>
             <ul className="collapseMenu">
               <a href="#" className="collapseSublink">
                 Data
@@ -39,25 +41,12 @@ function App() {
             </ul>
           </div>
           <div className="links">
-            <MessageOutlined className="linkIcon" />
-            <span className="linkName">Messenger</span>
-            <CaretDownOutlined className="collapseLink" />
-            <ul className="collapseMenu showCollapse">
-              <a href="#" className="collapseSublink">
-                Data
-              </a>
-              <a href="#" className="collapseSublink">
-                Group
-              </a>
-              <a href="#" className="collapseSublink">
-                Members
-              </a>
-            </ul>
-          </div>
-          <div className="links">
-            <PieChartOutlined className="linkIcon" />
-            <span className="linkName">Results</span>
-            <CaretDownOutlined className="collapseLink" />
+            <div className="linkName">
+              <MessageOutlined className="linkIcon" />
+              <span>Messenger</span>
+              <CaretDownOutlined className="collapseLink" />
+            </div>
+
             <ul className="collapseMenu">
               <a href="#" className="collapseSublink">
                 Data
@@ -71,9 +60,11 @@ function App() {
             </ul>
           </div>
           <div className="links">
-            <FolderOpenOutlined className="linkIcon" />
-            <span className="linkName">Projects</span>
-            <CaretDownOutlined className="collapseLink" />
+            <div className="linkName">
+              <PieChartOutlined className="linkIcon" />
+              <span>Results</span>
+              <CaretDownOutlined className="collapseLink" />
+            </div>
             <ul className="collapseMenu">
               <a href="#" className="collapseSublink">
                 Data
@@ -87,9 +78,11 @@ function App() {
             </ul>
           </div>
           <div className="links">
-            <TeamOutlined className="linkIcon" />
-            <span className="linkName">Team</span>
-            <CaretDownOutlined className="collapseLink" />
+            <div className="linkName">
+              <FolderOpenOutlined className="linkIcon" />
+              <span>Projects</span>
+              <CaretDownOutlined className="collapseLink" />
+            </div>
             <ul className="collapseMenu">
               <a href="#" className="collapseSublink">
                 Data
@@ -103,9 +96,29 @@ function App() {
             </ul>
           </div>
           <div className="links">
-            <SettingOutlined className="linkIcon" />
-            <span className="linkName">Settings</span>
-            <CaretDownOutlined className="collapseLink" />
+            <div className="linkName">
+              <TeamOutlined className="linkIcon" />
+              <span>Team</span>
+              <CaretDownOutlined className="collapseLink" />
+            </div>
+            <ul className="collapseMenu">
+              <a href="#" className="collapseSublink">
+                Data
+              </a>
+              <a href="#" className="collapseSublink">
+                Group
+              </a>
+              <a href="#" className="collapseSublink">
+                Members
+              </a>
+            </ul>
+          </div>
+          <div className="links">
+            <div className="linkName">
+              <SettingOutlined className="linkIcon" />
+              <span>Settings</span>
+              <CaretDownOutlined className="collapseLink" />
+            </div>
             <ul className="collapseMenu">
               <a href="#" className="collapseSublink">
                 Data
