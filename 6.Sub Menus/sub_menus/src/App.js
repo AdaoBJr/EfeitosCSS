0ec8585b3f788cb8372aca986c568ba3dab828e8
AdaoBJr <adaobjr@gmail.com>
2021-12-29 22:36:24 -0400

    feat: :sparkles: created basic structure

--- 6.Sub Menus/sub_menus/src/App.js
+++ 6.Sub Menus/sub_menus/src/App.js
@@ -3,7 +3,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <main>
+      <header>
+        <div className="header" />
+      </header>
+      <section>
+        <div className="section" />
+      </section>
+      <footer className="footer">
+        <div className="root">
+          <div></div>
+        </div>
+      </footer>
+    </main>
+  );
 }
 
 export default App;
