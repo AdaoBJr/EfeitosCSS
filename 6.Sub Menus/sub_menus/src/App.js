d11ca4a8477c7692cf3cf4bc57f0c8fea320c0d1
AdaoBJr <adaobjr@gmail.com>
2021-12-29 22:50:21 -0400

    feat: :construction: building links structure

--- 6.Sub Menus/sub_menus/src/App.js
+++ 6.Sub Menus/sub_menus/src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  CaretDownOutlined,
+  FolderOpenOutlined,
+  HomeOutlined,
+  MessageOutlined,
+  PieChartOutlined,
+  SettingOutlined,
+  TeamOutlined,
+} from '@ant-design/icons';
 
 import './App.css';
 
@@ -11,9 +20,29 @@ function App() {
       <section>
         <div className="section" />
       </section>
-      <footer className="footer">
-        <div className="root">
-          <div></div>
+      <footer className="root">
+        <div className="wrapper">
+          <div className="links">
+            <SettingOutlined className="linkIcon" />
+            <span className="linkName">Settings</span>
+            <CaretDownOutlined className="collapseLink" />
+            <ul className="collapseMenu">
+              <a href="#" className="collapseSublink">
+                Data
+              </a>
+              <a href="#" className="collapseSublink">
+                Group
+              </a>
+              <a href="#" className="collapseSublink">
+                Members
+              </a>
+            </ul>
+          </div>
+          <PieChartOutlined />
+          <MessageOutlined />
+          <FolderOpenOutlined />
+          <TeamOutlined />
+          <HomeOutlined />
         </div>
       </footer>
     </main>
