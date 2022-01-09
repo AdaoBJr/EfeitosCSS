949fbee78e9fd0876cdedac13639d516d9f93d40
AdaoBJr <adaobjr@gmail.com>
2022-01-09 16:16:57 -0400

    ttest

--- 6.Sub Menus/sub_menus/src/App.js
+++ 6.Sub Menus/sub_menus/src/App.js
@@ -14,7 +14,7 @@ import useApp from '../src/talons/useApp';
 import './App.css';
 
 function App() {
-  const { showCollapse, showElement } = useApp();
+  const { showCollapse, showElement, isDesktop } = useApp();
   return (
     <main>
       <header>
@@ -25,163 +25,54 @@ function App() {
       </section>
       <footer className="root">
         <div className="wrapper">
-          <div className="links">
+          <div className={showElement && showElement === 0 ? 'links move' : 'links'}>
             <div className="linkName">
               <HomeOutlined className="linkIcon" />
-              <button
-                type="button"
-                name="link-0"
-                className="linkBtn"
-                onClick={showCollapse}
-              >
+              <button type="button" name="0" className="linkBtn" onClick={showCollapse}>
                 Dashboard
               </button>
               <CaretDownOutlined
-                className={
-                  showElement === 'link-0' ? 'collapseLink rotate' : 'collapseLink'
-                }
+                className={showElement === 0 ? 'collapseLink rotate' : 'collapseLink'}
               />
             </div>
             <ul
-              className={
-                showElement === 'link-0' ? 'collapseMenu showCollapse' : 'collapseMenu'
-              }
+              className={showElement === 0 ? 'collapseMenu showCollapse' : 'collapseMenu'}
             >
               <a className="collapseSublink">Data</a>
               <a className="collapseSublink">Group</a>
               <a className="collapseSublink">Members</a>
             </ul>
           </div>
-          <div className="links">
-            <div className="linkName">
-              <MessageOutlined className="linkIcon" />
-              <button
-                type="button"
-                name="link-1"
-                className="linkBtn"
-                onClick={showCollapse}
-              >
-                Messenger
-              </button>
-              <CaretDownOutlined
-                className={
-                  showElement === 'link-1' ? 'collapseLink rotate' : 'collapseLink'
-                }
-              />
-            </div>
-
-            <ul
-              className={
-                showElement === 'link-1' ? 'collapseMenu showCollapse' : 'collapseMenu'
-              }
-            >
-              <a className="collapseSublink">Data</a>
-              <a className="collapseSublink">Group</a>
-              <a className="collapseSublink">Members</a>
-            </ul>
-          </div>
-          <div className="links">
-            <div className="linkName">
-              <PieChartOutlined className="linkIcon" />
-              <button
-                type="button"
-                name="link-2"
-                className="linkBtn"
-                onClick={showCollapse}
-              >
-                Results
-              </button>
-              <CaretDownOutlined
-                className={
-                  showElement === 'link-2' ? 'collapseLink rotate' : 'collapseLink'
-                }
-              />
-            </div>
-            <ul
-              className={
-                showElement === 'link-2' ? 'collapseMenu showCollapse' : 'collapseMenu'
-              }
-            >
-              <a className="collapseSublink">Data</a>
-              <a className="collapseSublink">Group</a>
-              <a className="collapseSublink">Members</a>
-            </ul>
-          </div>
-          <div className="links">
+          <div className={showElement && showElement <= 1 ? 'links move' : 'links'}>
             <div className="linkName">
               <FolderOpenOutlined className="linkIcon" />
-              <button
-                type="button"
-                name="link-3"
-                className="linkBtn"
-                onClick={showCollapse}
-              >
+              <button type="button" name="1" className="linkBtn" onClick={showCollapse}>
                 Projects
               </button>
               <CaretDownOutlined
-                className={
-                  showElement === 'link-3' ? 'collapseLink rotate' : 'collapseLink'
-                }
+                className={showElement === 1 ? 'collapseLink rotate' : 'collapseLink'}
               />
             </div>
             <ul
-              className={
-                showElement === 'link-3' ? 'collapseMenu showCollapse' : 'collapseMenu'
-              }
+              className={showElement === 1 ? 'collapseMenu showCollapse' : 'collapseMenu'}
             >
               <a className="collapseSublink">Data</a>
               <a className="collapseSublink">Group</a>
               <a className="collapseSublink">Members</a>
             </ul>
           </div>
-          <div className="links">
-            <div className="linkName">
-              <TeamOutlined className="linkIcon" />
-              <button
-                type="button"
-                name="link-4"
-                className="linkBtn"
-                onClick={showCollapse}
-              >
-                Team
-              </button>
-              <CaretDownOutlined
-                className={
-                  showElement === 'link-4' ? 'collapseLink rotate' : 'collapseLink'
-                }
-              />
-            </div>
-            <ul
-              className={
-                showElement === 'link-4' ? 'collapseMenu showCollapse' : 'collapseMenu'
-              }
-            >
-              <a className="collapseSublink">Data</a>
-              <a className="collapseSublink">Group</a>
-              <a className="collapseSublink">Members</a>
-            </ul>
-          </div>
-          <div className="links">
+          <div className={showElement && showElement <= 2 ? 'links move' : 'links'}>
             <div className="linkName">
               <SettingOutlined className="linkIcon" />
-              <button
-                type="button"
-                name="link-5"
-                className="linkBtn"
-                onClick={showCollapse}
-              >
+              <button type="button" name="2" className="linkBtn" onClick={showCollapse}>
                 Settings
               </button>
               <CaretDownOutlined
-                className={
-                  showElement === 'link-5' ? 'collapseLink rotate' : 'collapseLink'
-                }
+                className={showElement === 2 ? 'collapseLink rotate' : 'collapseLink'}
               />
             </div>
             <ul
-              className={
-                showElement === 'link-5' ? 'collapseMenu showCollapse' : 'collapseMenu'
-              }
+              className={showElement === 2 ? 'collapseMenu showCollapse' : 'collapseMenu'}
             >
               <a className="collapseSublink">Data</a>
               <a className="collapseSublink">Group</a>
--- 6.Sub Menus/sub_menus/src/talons/useApp.js
+++ 6.Sub Menus/sub_menus/src/talons/useApp.js
@@ -1,17 +1,36 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function useApp() {
-  const [showElement, setShowElement] = useState('');
+  const [showElement, setShowElement] = useState(undefined);
+  const [isDesktop, setIsDesktop] = useState(undefined);
 
   console.log('ELEMENT', showElement);
 
   const showCollapse = useCallback(
     ({ target: { name } }) => {
-      if (!showElement || name !== showElement) return setShowElement(name);
-      if (showElement) return setShowElement('');
+      const elementName = Number(name);
+      if (!showElement || elementName !== showElement) return setShowElement(elementName);
+      if (showElement) return setShowElement(undefined);
     },
     [showElement, setShowElement]
   );
 
-  return { showCollapse, showElement };
+  const checkWindowsSize = useCallback(() => {
+    const MIN_WIDTH = 641;
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= MIN_WIDTH) {
+      setIsDesktop(true);
+    } else {
+      setIsDesktop(false);
+    }
+  }, [isDesktop, setIsDesktop]);
+
+  // ---------------------------------------------------------------------------------------------
+  useEffect(checkWindowsSize, []);
+  window.addEventListener('resize', () => checkWindowsSize());
+
+  // ---------------------------------------------------------------------------------------------
+
+  return { showCollapse, showElement, isDesktop };
 }
